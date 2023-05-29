792bcb3b67f8ffaa38e0f8cd9de67e653541fc1d
Aurora_init <1114813380@qq.com>
2023-05-29 14:51:53 +0800

    更新图片下载的功能

--- 22_Red_Ant/software/_photo_download_part/photo.c
+++ 22_Red_Ant/software/_photo_download_part/photo.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
+#include <jansson.h> // 需要添加jansson库来解析JSON
 
 struct MemoryStruct {
     char* memory;
@@ -38,7 +39,7 @@ int main(void)
     curl = curl_easy_init();
     if (curl) {
         // 构造HTTP请求的URL
-        sprintf(url, "http://api.heclouds.com/devices/%s/datapoints?datastream_id=datastreams&limit=1", device_id);
+        sprintf(url, "http://api.heclouds.com/devices/%s/datapoints?datastream_id=datastreams&limit=100&page=10&per_page=100", device_id);
 
         // 设置HTTP请求的头信息
         struct curl_slist* headers = NULL;
@@ -55,7 +56,7 @@ int main(void)
 
         // 设置HTTP请求的回调函数，将响应数据存储在字符串中
         struct MemoryStruct chunk;
-        chunk.memory = malloc(1);
+        chunk.memory = malloc(2);
         chunk.size = 0;
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&chunk);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
@@ -65,27 +66,56 @@ int main(void)
         if (res != CURLE_OK) {
             fprintf(stderr, "Failed to download data: %s\n", curl_easy_strerror(res));
         } else {
-            // 将16进制字符串转换为二进制数据
-            hex_data = strstr(chunk.memory, "\"value\":\"") + 9;
-            hex_data[strcspn(hex_data, "\"")] = '\0';
-            int hex_len = strlen(hex_data);
-            unsigned char* binary_data = (unsigned char*) malloc(sizeof(unsigned char) * (hex_len / 2 + 1));
-            for (int i = 0; i < hex_len; i += 2) {
-                sscanf(&hex_data[i], "%02hhx", &binary_data[i / 2]);
-            }
-            binary_data[hex_len / 2] = '\0';
+            // 解析JSON响应以获取数据点数量
+            json_error_t error;
+            json_t* root = json_loads(chunk.memory, 0, &error);
 
-            // 将二进制数据写入文件
-            fp = fopen("downloaded_photo.jpg", "wb");
-            if (fp == NULL) {
-                fprintf(stderr, "Failed to open file\n");
-            } else {
-                fwrite(binary_data, sizeof(unsigned char), hex_len / 2, fp);
-                fclose(fp);
-            }
+			//printf("Received response: %s\n", chunk.memory); 
+			
+            json_t* data_points = json_object_get(root, "data");
+			json_t* datastreams = json_object_get(data_points, "datastreams");
 
-            // 释放缓冲区
-            free(binary_data);
+			json_t* datapoints = json_array_get(datastreams, 0);
+			json_t* datapointss = json_object_get(datapoints, "datapoints");
+
+			size_t num_data_points = json_array_size(datapointss);	//获取这个包含所有数据流的数组的长度								
+
+			for (size_t i = 0; i < num_data_points; i++)
+			{
+				json_t* value = json_array_get(datapointss, i);
+				json_t* values = json_object_get(value, "value");
+
+				json_t* datapointsss = json_array_get(values, 0);
+				json_t* datapointssss = json_object_get(datapointsss, "datapoints");
+				json_t* valuess = json_array_get(datapointssss, 0);
+
+				// 获取16进制字符串并转换为二进制数据
+				hex_data = (char*)json_string_value(json_object_get(valuess, "value"));
+				
+				int hex_len = strlen(hex_data);
+				unsigned char* binary_data = (unsigned char*) malloc(sizeof(unsigned char) * (hex_len / 2 + 1));
+				for (int j = 0; j < hex_len; j += 2) {
+					sscanf(&hex_data[j], "%02hhx", &binary_data[j / 2]);
+				}
+				binary_data[hex_len / 2] = '\0';
+
+				// 将二进制数据写入文件
+				char filename[64];
+				sprintf(filename, "downloaded_photo_%zu.jpg", i);
+				fp = fopen(filename, "wb");
+				if (fp == NULL) {
+					fprintf(stderr, "Failed to open file\n");
+				} else {
+					fwrite(binary_data, sizeof(unsigned char), hex_len / 2, fp);
+					fclose(fp);
+				}
+
+				// 释放缓冲区
+				free(binary_data);
+
+			}
+			// 释放JSON对象
+	        json_decref(root);
         }
 
         // 释放资源
